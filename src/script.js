9940c232be115d6115f415136d82faa6bb2d968a
Facundo Frau <51481449+facufrau@users.noreply.github.com>
2022-03-11 12:49:27 -0300

    Refactor showing and deleting in display

--- src/script.js
+++ src/script.js
@@ -14,8 +14,7 @@ function divide(number1, number2){
     return number1 / number2;
 }
 
-function operate(operator, operands) {
-    const [number1, number2] = operands;
+function operate(operator, number1, number2) {
     let result;
     if (operator === "+") {
         result = add(number1, number2);
@@ -32,19 +31,64 @@ function operate(operator, operands) {
 }
 
 const numbersButtons = document.querySelectorAll(".number");
+const operatorsButtons = document.querySelectorAll(".operator");
 const deleteButton = document.getElementById("del");
+const dotButton = document.getElementById("dot");
+const clearButton = document.getElementById("clear");
 
-numbersButtons.forEach(button => button.addEventListener("click", updateDisplay));
-deleteButton.addEventListener("click", deleteLastNumber);
+const objectValues = {
+    "display": undefined,
+    "operator": undefined,
+    "firstNumber": undefined,
+    "secondNumber": undefined,
+    "result": undefined,
+};
+
+numbersButtons.forEach(button => button.addEventListener("click", (e) => updateData(e, objectValues)));
+operatorsButtons.forEach(button => button.addEventListener("click", (e) => handleOperator(e, objectValues)))
+deleteButton.addEventListener("click", () => deleteLastNumber(objectValues));
 
 
-function updateDisplay(e) {
-    const value = e.target.value;
-    const display = document.getElementById("display");
-    display.textContent += value;
+function updateData(e, dataObject) {
+    const numberPressed = e.target.value;
+    if (!dataObject.display) {
+        dataObject.display = "" + numberPressed;
+    } else {
+        dataObject.display = dataObject.display + numberPressed;
+    }
+    updateDisplay(dataObject.display);
 }
 
-function deleteLastNumber(e) {
+function updateDisplay(numberToDisplay) {
     const display = document.getElementById("display");
-    display.textContent = display.textContent.slice(0,-1);
+    display.textContent = numberToDisplay;
+}
+
+function deleteLastNumber(dataObject) {
+    dataObject.display = dataObject.display.slice(0,-1);
+    updateDisplay(dataObject.display);
+}
+
+function isSecondNumber(dataObject){
+    return dataObject.firstNumber !== undefined;
+}
+
+
+function handleOperator(e, dataObject){
+    const operator = e.target.value;
+    const firstNumber = dataObject.firstNumber;
+    const secondNumber = dataObject.secondNumber;
+
+    if (operator === "=" && isSecondNumber(dataObject)) {
+        dataObject.secondNumber = Number(dataObject.display);
+        const result = operate(dataObject.operator, firstNumber, secondNumber)
+        dataObject.result = result;
+        updateDisplay(result);
+    } else { 
+        dataObject.operator = operator;
+        dataObject.firstNumber = Number(dataObject.display);
+        dataObject.display = "";
+        updateDisplay(dataObject.display);        
+    }
+
 }
