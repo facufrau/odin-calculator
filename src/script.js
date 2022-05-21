34126180cd1f28721e139f27f51e36861a1825e7
Facundo Frau <facufrau@gmail.com>
2022-05-20 22:22:41 -0300

    Complete basic funcionality rework

--- src/script.js
+++ src/script.js
@@ -45,11 +45,17 @@ function updateDisplay(number, calculatorData) {
 
 }
 
+function clearDisplay(calculatorData) {
+    const display = document.getElementById("display");
+    calculatorData.display = "0";
+    display.textContent = calculatorData.display;
+}
+
 function clearAll(calculatorData){
     for (let data in calculatorData) {
         data === "display" ? calculatorData[data] = 0 : calculatorData[data] = null;
     };
-    document.getElementById("display").textContent = "0";
+    clearDisplay(calculatorData);
 }
 
 const calculatorData = {
@@ -64,13 +70,28 @@ buttons.forEach (button => button.addEventListener("click", (e) => buttonClick(e
 
 function buttonClick(e, calculatorData) {
     let targetName = e.target.name;
+    let result;
+    const display = document.getElementById("display");
 
     if (targetName === "number") {
         let number = e.target.value;
         updateDisplay(number, calculatorData);
         } 
     else if (targetName === "operator") {
-        console.log("operator");
+        let operator = e.target.value;
+        if(!calculatorData.firstNumber) {
+            calculatorData.firstNumber = Number(calculatorData.display);
+            calculatorData.operator = operator;
+            clearDisplay(calculatorData);
+            }
+        else {
+            calculatorData.secondNumber = Number(calculatorData.display);
+            result = operate(calculatorData.operator, calculatorData.firstNumber, calculatorData.secondNumber);
+            display.textContent = result;
+            calculatorData.firstNumber = result;
+            calculatorData.operator = operator;
+            calculatorData.display = "0";
+            }
         } 
     else if (targetName === "clear") {
         clearAll(calculatorData);
